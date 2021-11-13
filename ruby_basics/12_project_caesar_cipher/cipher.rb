0b6b823fa6ed916294c6855a8e25c6c2f6730c59
lea927 <lxthym@icloud.com>
2021-11-13 20:13:12 +0800

    Creates caesar cipher exercise [WIP]

---
+++ ruby_basics/12_project_caesar_cipher/cipher.rb
@@ -0,0 +1,20 @@
+require 'pry-byebug'
+
+def caesar_cipher(string, key)
+  alpha_arr = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+  index_arr = []
+  # get index of each character from string
+  # push index into a new array
+  # encrypt new array using the key
+  # use Z - A looping; when array reaches letter Z, it will resume counting from A
+  # create contraints; A.) User enters non-letter characters
+  string.split.each do |s|
+    alpha_arr.each_with_index do |letter,index|
+      index_arr << index if s.upcase === letter
+      binding.pry
+    end
+    p index_arr
+  end
+end
+
+caesar_cipher('b', 1)
