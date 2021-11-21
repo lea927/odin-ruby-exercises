5f87fe7d6a2cb90a733b5cc29549b83b8121667c
lea927 <lxthym@icloud.com>
2021-11-21 16:54:32 +0800

    Re-factors code and used map and gsub methods

--- ruby_basics/12_project_caesar_cipher/cipher.rb
+++ ruby_basics/12_project_caesar_cipher/cipher.rb
@@ -1,7 +1,8 @@
 require 'pry-byebug'
 
-def caesar_cipher(string, key)
-  alpha_arr = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+def caesar_cipher(input, key)
+  @alphabet = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+  # alpha_arr_2 = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
   index_arr = []
   # get index of each character from string
   # ignores whitespace
@@ -9,61 +10,19 @@ def caesar_cipher(string, key)
   # encrypt new array using the key
   # use Z - A looping; when array reaches letter Z, it will resume counting from A
   # create contraints; A.) User enters non-letter characters
-  numbers = {
-   "a" => 1,
-   "b" => 2, 
-   "c" => 3, 
-   "d" => 4, 
-   "e" => 5, 
-   "f" => 6, 
-   "g" => 7, 
-   "h" => 8, 
-   "i" => 9, 
-   "j" => 10, 
-   "k" => 11, 
-   "l" => 12, 
-   "m" => 13, 
-   "n" => 14,
-   "0" => 15, 
-   "p" => 16, 
-   "q" => 17, 
-   "r" => 18, 
-   "s" => 19, 
-   "t" => 20, 
-   "u" => 21, 
-   "v" => 22, 
-   "w" => 23, 
-   "x" => 24, 
-   "y" => 25, 
-   "z" => 26, 
-   "A" => 1, 
-   "B" => 2, 
-   "C" => 3, 
-   "D" => 4, 
-   "E" => 5, 
-   "F" => 6, 
-   "G" => 7, 
-   "H" => 8, 
-   "I" => 9, 
-   "J" => 10, 
-   "K" => 11, 
-   "L" => 12, 
-   "M" => 13, 
-   "N" => 14,
-   "O" => 15, 
-   "P" => 16, 
-   "Q" => 17, 
-   "R" => 18, 
-   "S" => 19, 
-   "T" => 20, 
-   "U" => 21, 
-   "V" => 22, 
-   "W" => 23, 
-   "X" => 24, 
-   "Y" => 25, 
-   "Z" => 26, 
+  encrypted_string = input.split("").map { |content|
+    content.gsub(/\w/) { |string|
+      def convert(string) 
+        @alphabet.each_with_index do |letter,index|
+          if string.casecmp? letter
+            return index
+          end
+        end
+      end
+      string = convert(string)
+    }
   }
-  p string.gsub(/\w/,numbers)
+  p encrypted_string
 end
 
-caesar_cipher('a A b', 1)
+caesar_cipher('Bac a', 2)
