38ce4184e80c36d0f85d87d2092aca2865303028
lea927 <lxthym@icloud.com>
2021-11-24 00:04:01 +0800

    Creates boolean method to check if parameter is lowercase [WIP]

--- ruby_basics/12_project_caesar_cipher/cipher.rb
+++ ruby_basics/12_project_caesar_cipher/cipher.rb
@@ -2,14 +2,20 @@
 # keep the same case
 require 'pry-byebug'
 
+def is_lower?(letter)
+  return true if letter =~ /[a-z]/ 
+  return false if letter !=~ /[a-z]/
+end
+
 def caesar_cipher(input, key)
   @alphabet = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
   @key = key
+  @input = input
   encrypted = input.split("").map { |content|
     content.gsub(/\w/) { |string|
       def convert(string) 
         @alphabet.each_with_index do |letter,index|
-          if string.casecmp? letter
+          if string.casecmp? letter 
             position = index + @key
             position = position - (@alphabet.length) if position > 25
             return @alphabet[position]
@@ -18,8 +24,10 @@ def caesar_cipher(input, key)
       end
       string = convert(string)
     }
+  }  
+  p retain_case = encrypted.join('').gsub(/\w/) { |letter|
+    
   }
-  p encrypted
 end
 
 caesar_cipher("What a string!", 5)
